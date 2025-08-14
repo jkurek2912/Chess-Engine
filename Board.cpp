f8f02bddec2739dcf557fab4f39883d9ea95dba0
Jacob <jkurek2912@gmail.com>
2025-08-14 18:15:22 -0500

    board representation

---
+++ Board.cpp
@@ -0,0 +1,20 @@
+class Board {
+    long whitePawns;
+    long whiteKnights;
+    long whiteBishops;
+    long whiteRooks;
+    long whiteQueens;
+    long whiteKing;
+    long blackPawns;
+    long blackKnights;
+    long blackBishops;
+    long blackRooks;
+    long blackQueens;
+    long blackKing;
+
+    long whiteEnPassantTargets;
+    long blackEnPassantTargets;
+    bool whiteCastle;
+    bool blackCastle;
+    bool whiteToMove;
+};

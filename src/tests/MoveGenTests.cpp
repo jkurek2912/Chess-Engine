739ef7a11c71c9e1f41c5a02611ef366ba9fa740
Jacob <jkurek2912@gmail.com>
2025-10-10 16:23:25 -0400

    pawn move tests

--- src/tests/MoveGenTests.cpp
+++ src/tests/MoveGenTests.cpp
@@ -4,12 +4,53 @@
 
 #include <gtest/gtest.h>
 
-TEST(MoveGen, PawnPush)
+// ----------------- Pawn Tests -----------------
+TEST(MoveGen, WhitePawnPush)
 {
     Board board;
     board.setCustomBoard("8/8/8/3P4/8/8/8/8 w - - 0 1");
     MoveGen::generateLegalMoves(board);
     EXPECT_EQ(board.legalMoves.size(), 1u);
+}
+
+TEST(MoveGen, BlackPawnPush)
+{
+    Board board;
+    board.setCustomBoard("8/8/8/4p3/8/8/8/8 b - - 0 1");
+    MoveGen::generateLegalMoves(board);
+    EXPECT_EQ(board.legalMoves.size(), 1u);
+}
+
+TEST(MoveGen, WhitePawnDoublePush)
+{
+    Board board;
+    board.setCustomBoard("8/8/8/8/8/8/4P3/8 w - - 0 1");
+    MoveGen::generateLegalMoves(board);
+    EXPECT_EQ(board.legalMoves.size(), 2u);
+}
+
+TEST(MoveGen, BlackPawnDoublePush)
+{
+    Board board;
+    board.setCustomBoard("8/4p3/8/8/8/8/8/8 b - - 0 1");
+    MoveGen::generateLegalMoves(board);
+    EXPECT_EQ(board.legalMoves.size(), 2u);
+}
+
+TEST(MoveGen, WhitePawnBlocked)
+{
+    Board board;
+    board.setCustomBoard("8/8/8/4n3/4P3/8/8/8 w - - 0 1");
+    MoveGen::generateLegalMoves(board);
+    EXPECT_TRUE(board.legalMoves.empty);
+}
+
+TEST(MoveGen, BlackPawnBlocked)
+{
+    Board board;
+    board.setCustomBoard("8/8/8/4p3/4N3/8/8/8 b - - 0 1");
+    MoveGen::generateLegalMoves(board);
+    EXPECT_TRUE(board.legalMoves.empty);
 }
 
 // ----------------- Check Tests -----------------
@@ -25,9 +66,9 @@ TEST(MoveGen, WhitePawnCheckingKing)
 TEST(MoveGen, BlackPawnCheckingKing)
 {
     Board board;
-    board.setCustomBoard("4k3/8/8/8/8/8/3p4/4K3 w - - 0 1");
+    board.setCustomBoard("4k3/8/8/8/8/8/3p4/4K3 b - - 0 1");
     EXPECT_TRUE(MoveGen::inCheck(board, WHITE));
-    board.setCustomBoard("4k3/8/8/8/8/8/5p2/4K3 w - - 0 1");
+    board.setCustomBoard("4k3/8/8/8/8/8/5p2/4K3 b - - 0 1");
     EXPECT_TRUE(MoveGen::inCheck(board, WHITE));
 }
 
@@ -43,16 +84,16 @@ TEST(MoveGen, WhiteBishopCheckingKing)
 TEST(MoveGen, BlackBishopCheckingKing)
 {
     Board board;
-    board.setCustomBoard("4k3/8/8/b7/8/8/8/4K3 w - - 0 1");
+    board.setCustomBoard("4k3/8/8/b7/8/8/8/4K3 b - - 0 1");
     EXPECT_TRUE(MoveGen::inCheck(board, WHITE));
-    board.setCustomBoard("4k3/8/8/8/7b/8/8/4K3 w - - 0 1");
+    board.setCustomBoard("4k3/8/8/8/7b/8/8/4K3 b - - 0 1");
     EXPECT_TRUE(MoveGen::inCheck(board, WHITE));
 }
 
 TEST(MoveGen, WhiteQueenCheckingKing)
 {
     Board board;
-    board.setCustomBoard("4k3/8/8/8/8/8/4Q3/4K3 b - - 0 1");
+    board.setCustomBoard("4k3/8/8/8/8/8/4Q3/4K3 w - - 0 1");
     EXPECT_TRUE(MoveGen::inCheck(board, BLACK));
     board.setCustomBoard("4k3/8/8/8/Q7/8/8/4K3 w - - 0 1");
     EXPECT_TRUE(MoveGen::inCheck(board, BLACK));
@@ -69,17 +110,36 @@ TEST(MoveGen, WhiteQueenCheckingKing)
 TEST(MoveGen, BlackQueenCheckingKing)
 {
     Board board;
-    board.setCustomBoard("4k3/4q3/8/8/8/8/8/4K3 w - - 0 1");
+    board.setCustomBoard("4k3/4q3/8/8/8/8/8/4K3 b - - 0 1");
     EXPECT_TRUE(MoveGen::inCheck(board, WHITE));
-    board.setCustomBoard("4k3/8/8/q7/8/8/8/4K3 w - - 0 1");
+    board.setCustomBoard("4k3/8/8/q7/8/8/8/4K3 b - - 0 1");
     EXPECT_TRUE(MoveGen::inCheck(board, WHITE));
-    board.setCustomBoard("4k3/8/8/8/7q/8/8/4K3 w - - 0 1");
+    board.setCustomBoard("4k3/8/8/8/7q/8/8/4K3 b - - 0 1");
     EXPECT_TRUE(MoveGen::inCheck(board, WHITE));
-    board.setCustomBoard("4k3/8/8/8/8/8/8/4K2q w - - 0 1");
+    board.setCustomBoard("4k3/8/8/8/8/8/8/4K2q b - - 0 1");
     EXPECT_TRUE(MoveGen::inCheck(board, WHITE));
-    board.setCustomBoard("4k3/8/8/8/8/8/8/q3K3 w - - 0 1");
+    board.setCustomBoard("4k3/8/8/8/8/8/8/q3K3 b - - 0 1");
     EXPECT_TRUE(MoveGen::inCheck(board, WHITE));
-    board.setCustomBoard("4k3/8/8/8/4K3/8/8/4q3 w - - 0 1");
+    board.setCustomBoard("4k3/8/8/8/4K3/8/8/4q3 b - - 0 1");
+    EXPECT_TRUE(MoveGen::inCheck(board, WHITE));
+}
+
+// ----------------- Check Mate Tests -----------------
+TEST(MoveGen, CheckMate1)
+{
+    Board board;
+    board.setCustomBoard("1R3k2/R7/8/8/8/8/8/4K3 b - - 0 1");
+    MoveGen::generateLegalMoves(board);
+    EXPECT_TRUE(board.legalMoves.empty());
+    EXPECT_TRUE(MoveGen::inCheck(board, BLACK));
+}
+
+TEST(MoveGen, CheckMate2)
+{
+    Board board;
+    board.setCustomBoard("5k2/8/8/8/8/8/r7/1r2K3 w - - 0 1");
+    MoveGen::generateLegalMoves(board);
+    EXPECT_TRUE(board.legalMoves.empty());
     EXPECT_TRUE(MoveGen::inCheck(board, WHITE));
 }
 
